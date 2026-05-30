4b9e42915e13cb68e708a16cbcb2125e8dbc8173
Meli <ms.melcastro@gmail.com>
2026-05-30 11:30:43 -0700

    steps 8 - 9

--- js/oop-practice.js
+++ js/oop-practice.js
@@ -49,7 +49,7 @@
 //     }
 // }
 
-// const dog = new AnimalInfo('Dog', 'Aussie', 'Grey', 32, 48);
+// const dog = new AnimalInfo('Dog', 'Pitbull', 'Brown', 32, 48);
 
 // STEP 6
 // const Animal = function (init_type, init_breed, init_color, init_height, init_length) {
@@ -59,14 +59,89 @@
 //     this.height = init_height;
 //     this.length = init_length;
 // };
-// const dog = new Animal('Dog', 'Aussie', 'Grey', 32, 48);
+// const dog = new Animal('Dog', 'Pitbull', 'Brown', 32, 48);
 
 // for (let property in dog) {
 //     console.log(property + ': ' + dog[property]);
 // }
 
 // STEP 7
+// class Animal {
+
+//     constructor(type, breed, color, height, length) {
+//         this.type = type;
+//         this.breed = breed;
+//         this.color = color;
+//         this.height = height;
+//         this.length = length;
+//     }
+
+//     speak() {
+
+//         if (this.type.toLowerCase() === 'dog') {
+//             return `The ${this.color} dog is barking!`;
+//         }
+
+//         return `The ${this.color} cat is meowing!`;
+
+//     }
+
+// }
+
+// const dog = new Animal('Dog', 'Pitbull', 'Brown', 32, 48);
+
+// console.log(dog.speak());
 
 // STEP 8
+// class Animal {
+
+//     constructor(type, breed, color, height, length) {
+
+//         let Type = type;
+//         let Breed = breed;
+//         let Color = color;
+//         let Height = height;
+//         let Length = length;
+
+//         function checkType() {
+
+//             if (Type.toLowerCase() === 'dog') {
+//                 return 'dog';
+//             } else {
+//                 return 'cat';
+//             }
+
+//         }
+
+//         this.speak = function () {
+//             return `The ${checkType()} has made a noise!`;
+//         };
+
+//     }
+
+// }
+
+// const pet = new Animal('Dog', 'Pitbull', 'Brown', 36, 58);
+
+// console.log(pet.speak());
 
 // STEP 9
+// String.prototype.findWords = function (word) {
+
+//     let words = this.split(' ');
+//     let count = 0;
+
+//     for (let i = 0; i < words.length; i++) {
+
+//         if (words[i].toLowerCase() === word.toLowerCase()) {
+//             count++;
+//         }
+
+//     }
+
+//     alert(`The word "${word}" was found ${count} times.`);
+// };
+
+// let message = 'The Dog cat got in a fight with another dog. Then the DOG jumped over a fence.';
+
+// message.findWords('dog');
