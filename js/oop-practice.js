16408ec5f5da074a33066b543adcda82873a2b03
Meli <ms.melcastro@gmail.com>
2026-05-30 09:10:38 -0700

    steps 1 & 2

--- js/oop-practice.js
+++ js/oop-practice.js
@@ -1,6 +1,15 @@
 // STEP 1
+// function Cat() {
+//     console.log('Cat');
+// }
+
+// const Dog = function() {
+//     console.log('Dog');
+// };
 
 // STEP 2
+// const cat1 = new Cat();
+// const dog1 = new Dog();
 
 // STEP 3
 
@@ -9,6 +18,7 @@
 // STEP 5
 
 // STEP 6
+
 
 // STEP 7
 
