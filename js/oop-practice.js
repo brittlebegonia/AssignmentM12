90f9406ed32b1f555c3fd03ec4bff7d8495ad062
Meli <ms.melcastro@gmail.com>
2026-05-30 09:45:10 -0700

    step 3 - 6

--- js/oop-practice.js
+++ js/oop-practice.js
@@ -12,13 +12,58 @@
 // const dog1 = new Dog();
 
 // STEP 3
+// class Animal {
+//     create() {
+//         console.log('The Animal has been created');
+//     }
+// }
+
+// const animal = new Animal();
+// animal.create();
 
 // STEP 4
+// class Animal {
+
+//     constructor(message) {
+//         this.message = message;
+//     }
+
+//     create() {
+//         console.log(this.message);
+//     }
+
+// }
+
+// const animal = new Animal('The Animal has been created');
+
+// animal.create();
 
 // STEP 5
+// class AnimalInfo {
+//     constructor(type, breed, color, height, length) {
+//         this.type = type;
+//         this.breed = breed;
+//         this.color = color;
+//         this.height = height;
+//         this.length = length;
+//     }
+// }
+
+// const dog = new AnimalInfo('Dog', 'Aussie', 'Grey', 32, 48);
 
 // STEP 6
+// const Animal = function (init_type, init_breed, init_color, init_height, init_length) {
+//     this.type = init_type;
+//     this.breed = init_breed;
+//     this.color = init_color;
+//     this.height = init_height;
+//     this.length = init_length;
+// };
+// const dog = new Animal('Dog', 'Aussie', 'Grey', 32, 48);
 
+// for (let property in dog) {
+//     console.log(property + ': ' + dog[property]);
+// }
 
 // STEP 7
 
