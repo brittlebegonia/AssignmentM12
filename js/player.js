342745a06a09d657713baf75256cc865ded5b4cf
Meli <ms.melcastro@gmail.com>
2026-05-30 14:40:56 -0700

    player

--- js/player.js
+++ js/player.js
@@ -1,51 +1,84 @@
-class Jukebox {
-    constructor(albums) {
-        this.albums = []
+class Player {
+    constructor() {
+        this.albums = [];
     }
-    addAlbum = function(album) {
-        this.albums.push(album)
+
+    addAlbum(album) {
+        this.albums.push(album);
     }
-    favoriteAlbum = function() {
-        let max = -1, fav
+
+    favoriteAlbum() {
+        let max = -1;
+        let fav = null;
+
         for (let i = 0; i < this.albums.length; i++) {
             if (this.albums[i].played > max) {
-                max = this.albums[i].played
-                fav = this.albums[i]
+                max = this.albums[i].played;
+                fav = this.albums[i];
             }
         }
-        return fav.display()
+
+        return fav;
+    }
+
+    returnAlbum(index) {
+        return this.albums[index];
     }
 }
 
 class Album {
     constructor(artist, title) {
-        this.artist = artist
-        this.title = title
-        this.played = 0
+        this.artist = artist;
+        this.title = title;
+        this.played = 0;
     }
-    play = function() {
-        this.played += 1
+
+    play() {
+        this.played++;
     }
-    display = function() {
-        return `${this.artist} : ${this.title}. The album has been played ${this.played} times.`
+
+    display() {
+        return `${this.artist} : ${this.title} (Played ${this.played} times)`;
     }
 }
 
-var jbox = new Jukebox()
-const album1 = new Album('Operation Ivy', 'Energy')
-const album2 = new Album('Blink 182', 'Dude Ranch')
-const album3 = new Album('New Found Glory', 'Sticks and Stones')
+window.addEventListener("load", () => {
 
-jbox.addAlbum(album1)
-jbox.addAlbum(album2)
-jbox.addAlbum(album3)
+    const player = new Player();
 
-album1.play()
-album2.play()
-album2.play()
-album2.play()
-album2.play()
-album2.play()
-album3.play()
+    const albumData = [
+        ['Operation Ivy', 'Energy'],
+        ['Blink 182', 'Dude Ranch'],
+        ['New Found Glory', 'Sticks and Stones']
+    ];
 
-console.log(`Your favorite album is: ${jbox.favoriteAlbum()}`)
+    const dropdown = document.getElementById("albums");
+    const playBtn = document.getElementById("play");
+    const favBtn = document.getElementById("favorite");
+    const output = document.getElementById("favoriteAlbum");
+
+    // DROPDOWN
+    for (let i = 0; i < albumData.length; i++) {
+
+        const album = new Album(albumData[i][0], albumData[i][1]);
+        player.addAlbum(album);
+
+        dropdown.add(new Option(`${album.artist} - ${album.title}`, i));
+    }
+
+    // PLAY BUTTON
+    playBtn.addEventListener("click", () => {
+        const selectedIndex = dropdown.value;
+        player.returnAlbum(selectedIndex).play();
+    });
+
+    // FAVORITE BUTTON
+    favBtn.addEventListener("click", () => {
+        const fav = player.favoriteAlbum();
+
+        if (fav) {
+            output.innerHTML = `Your favorite album is ${fav.display()}`;
+        }
+    });
+
+});
